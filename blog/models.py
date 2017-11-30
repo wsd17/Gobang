675d25f5f2f9e5a47ad722b1a69aa7e1fe42f707
Byoung-Hun Ko <wsd17@naver.com>
2017-11-30 21:14:12 +0900

    댓글 기능 구현

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+from django.core.urlresolvers import reverse
 
 
 class Post(models.Model):
@@ -11,6 +13,11 @@ class Post(models.Model):
 	def __str__(self):
 		return self.title
 
+
 class Comment(models.Model):
 	post = models.ForeignKey(Post)
+	author = models.ForeignKey(settings.AUTH_USER_MODEL)
 	message = models.TextField()
+
+	def get_absolute_url(self):
+		return reverse('blog:post_detail', args=[self.post_id])
